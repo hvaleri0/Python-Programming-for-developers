03729bbd79de84258860091e055e182a64dedaf9
hvaleri0 <46056177+hvaleri0@users.noreply.github.com>
2021-01-17 15:41:30 -0500

    Arithmetic operations

---
+++ arithmetic _operations.py
@@ -0,0 +1,12 @@
+x = 10 + 3
+x = 10 - 3
+x = 10 * 3
+x = 10 / 3
+x = 10 // 3
+x = 10 % 3
+x = 10 ** 3
+
+
+x = x + 1
+x += 1  # Augmented asigment operator, can be used with all above operations
+print(x)
