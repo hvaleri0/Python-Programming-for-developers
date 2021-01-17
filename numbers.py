6d9c38198073b77788af0b5a7f8c8a516877cd4f
hvaleri0 <46056177+hvaleri0@users.noreply.github.com>
2021-01-17 15:28:44 -0500

    Numbers

---
+++ numbers.py
@@ -0,0 +1,11 @@
+x = 10
+
+x = 0b10
+print(bin(x))  # bin()  -> function use to show the binary rep of a number
+
+x = 0x12
+print(hex(x))  # bin()  -> function use to show the binary rep of a number
+
+# a + bi
+y: complex = 1 + 2j
+print(y)
