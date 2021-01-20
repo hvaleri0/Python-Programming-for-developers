e3e6ee4a9efff918c8b9459e60e1a0c27278ea21
hvaleri0 <46056177+hvaleri0@users.noreply.github.com>
2021-01-19 23:29:10 -0500

    Conditional Statement

---
+++ conditional_statements.py
@@ -0,0 +1,16 @@
+# If statement keywords: if, elif, else
+age = 1
+if age >= 18:
+    print("Adult")
+    print("Adult")
+elif age >= 13:
+    print("Teenager")
+else:
+    print("Child")
+
+print("All done!")
+
+if condition:
+    pass  # Use for empty Blocks
+else:
+    pass
