9cd7a7821946adbe65aff3e1a2ac9d5140ae0f84
hvaleri0 <46056177+hvaleri0@users.noreply.github.com>
2021-01-19 23:16:43 -0500

    Type conversion

---
+++ type_conversion.py
@@ -0,0 +1,16 @@
+x = input("x: ")
+y = x + 1
+
+# Conversions
+print(int(x))
+print(float(x))
+print(bool(x))  # Falsy Truthy Concept
+print(int(x))
+
+# Falsy:
+# ""
+# 0
+# []
+# None (same as null)
+
+# everything else is truthy
