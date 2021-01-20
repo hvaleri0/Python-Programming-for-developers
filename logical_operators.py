668e806d8a9e23798dd707e2a6f686eccdefd2e6
hvaleri0 <46056177+hvaleri0@users.noreply.github.com>
2021-01-19 23:43:48 -0500

    Logical Operators

---
+++ logical_operators.py
@@ -0,0 +1,9 @@
+name = " "  # space is not an empty string
+if not name.strip():  # to fix whhite sapce use stip
+    print('Name is empty')
+
+
+age = 22
+# if age >= 18 and age < 65:
+if 18 <= age < 65:
+    print("Eligible")
