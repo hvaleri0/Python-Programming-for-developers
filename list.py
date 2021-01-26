d21ea313b0d8af63efce7457f9404aca975eaad8
hvaleri0 <46056177+hvaleri0@users.noreply.github.com>
2021-01-25 19:52:21 -0500

    list

---
+++ list.py
@@ -0,0 +1,12 @@
+letters = ["a", "b", "c"]
+matrix = [[1, 2], [3, 4]]
+
+zeros = [0] * 5  # use asterik to clone 5 times
+
+combine = zeros + letters  # use plus to concatanate
+
+# use list & range function to iterate numbers sequentially
+numbers = list(range(20))
+
+chars = list("Hello World")  # use list to iterate over string as well
+print(chars)
