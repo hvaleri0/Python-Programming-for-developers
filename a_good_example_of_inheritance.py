545f9fcd1710eea5e0bcac8bac11198c619c9a43
hvaleri0 <46056177+hvaleri0@users.noreply.github.com>
2021-03-05 16:48:33 -0500

    A good example of inheritance

---
+++ a_good_example_of_inheritance.py
@@ -0,0 +1,34 @@
+# By Convention always end exception name with "Error" and base it it with Exception
+class InvalidOperationError(Exception):
+    pass
+
+
+class Stream:
+    def __init__(self):
+        self.opened = False
+
+    def open(self):
+        if self.opened:
+            raise InvalidOperationError("Stream is already opened")
+        self.opened = True
+
+    def close(self):
+        if not self.opened:
+            raise InvalidOperationError("Stream is already closed")
+        self.opened = False
+
+
+class FileStream(Stream):
+    def read(self):
+        print('Reading data from a file.')
+
+
+class NetworkStream(Stream):
+    def read(self):
+        print('Reading data from a network.')
+
+
+m = NetworkStream()
+m.open()
+m.close()
+m.read()
