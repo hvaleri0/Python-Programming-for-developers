87418011a4b41723c5fc076b5c734ef6886e802a
hvaleri0 <46056177+hvaleri0@users.noreply.github.com>
2021-03-07 13:07:21 -0500

    The Dir Function

---
+++ Modules/The dir Function/app.py
@@ -0,0 +1,6 @@
+from ecommerce.shopping import sales
+
+# print(dir(sales)) # to get the directory once you have the directories you can print information to debug
+print(sales.__name__)
+print(sales.__package__)
+print(sales.__file__)
