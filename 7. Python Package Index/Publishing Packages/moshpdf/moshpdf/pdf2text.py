abc2f62ec7103286000bcc05b187c6d134093551
hvaleri0 <46056177+hvaleri0@users.noreply.github.com>
2021-03-17 14:43:07 -0400

    Doc Strings

--- 7. Python Package Index/Publishing Packages/moshpdf/moshpdf/pdf2text.py
+++ 7. Python Package Index/Publishing Packages/moshpdf/moshpdf/pdf2text.py
@@ -1,2 +1,16 @@
-def convert():
-    print("pdf2text")
+""" This module provides functions to convert a PDF to test """
+
+
+class Converter:
+    """ A simple converter for converting PDFs to text."""
+
+    def convert(self, path):
+        """ Convert the given PDF to text
+
+        Parameter:
+        path (str): The path to a PDF file.
+
+        Returns:
+        str: The content of the PDF file as text
+        """
+        print("pdf2text")
