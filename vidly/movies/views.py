459f353e5989cbf4285344c26a0cc80213f31318
hvaleri0 <46056177+hvaleri0@users.noreply.github.com>
2021-04-08 08:32:21 -0400

    Database Abstraction API

--- vidly/movies/views.py
+++ vidly/movies/views.py
@@ -1,8 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    # Select all the item in the table - # Select * FROM movies_movie:
+    movies = Movie.objects.all()
+
+    # Filter Items in the table - Select * FROM movies_movie WHERE release_year=1984:
+    # Movie.objects.filter(release_year=1984)
+
+    # Get one Item in the table - Select * FROM movies_movie WHERE ...:
+    # Movie.objects.get(id=1)
+
+    output = ', '.join([m.title for m in movies])
+
+    return HttpResponse(output)
