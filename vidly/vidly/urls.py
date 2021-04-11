1a8cc8e2de108d299ae73d55bf24fc70eadc376b
hvaleri0 <46056177+hvaleri0@users.noreply.github.com>
2021-04-11 16:32:07 -0400

    Adding the Homepage

--- vidly/vidly/urls.py
+++ vidly/vidly/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from api.models import MovieResource
+from . import views
 
 movie_resource = MovieResource()
 
 urlpatterns = [
+    path('', views.home),
     path('admin/', admin.site.urls),
     # no need to add .py django handles it
     path('movies/', include('movies.urls')),
